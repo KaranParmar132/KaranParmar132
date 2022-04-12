bfbdfcb8bb4a3db4c208add85009888521107be9
MekaDeveloper <meka.developer@gmail.com>
2022-04-12 21:34:48 +0530

    Homepage Update
    Homepage Update 12-4-22

---
+++ assets/custom.js
@@ -0,0 +1,11 @@
+// //console.log("console_clicked");
+// window.addEventListener("load", function(){
+  
+// const fade_slide = document.querySelector(".slider-counter__link");
+//   fade_slide.addEventListener('click', function () { 
+//      var element = document.querySelector(".slider__fade");
+//      var haselement = element.classList.contains('active');
+//        element.classList.remove("active");
+//        element.classList.add("active");
+//   });
+// });
